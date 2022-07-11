a516a0e2ecb77944fb106dea296d701824f45924
mgtmh991013 <>
2022-07-11 09:03:09 +0000

    1 weak greedy ex

---
+++ 1_weak_greedy/ex02.py
@@ -0,0 +1,25 @@
+# 큰 수의 법칙
+
+# 입력값
+# 배열의 크기 n
+# 숫자 더하는 횟수 m
+# 특정 인덱스 값의 중복 더하기 제한 횟수 k 
+# N 개의 자연수
+
+n, m, k=map(int,input("입력해라").split())
+data= list(map(int, input().split()))
+result=0
+data.sort(reverse=True) # 내림차순 정렬
+print(data)
+
+while m!=0: 
+    for i in range(k): # 중복 가능 횟수까지 가장 큰 값 더함
+        result+=data[0]
+        m-=1 
+        if(m==0):
+            break
+
+    result+=data[1] # 두번째로 큰값 더함
+    m-=1 # 값을 더할때마다 카운팅 해줌
+
+print(result)
---
+++ 1_weak_greedy/ex03.py
@@ -0,0 +1,15 @@
+#숫자 카드 게임
+
+row, col=map(int,input("2개 입력해라").split()) # 행 열 입력
+result=[[0]*row for _ in range(col)] # 2차원 배열 초기화
+max=0
+
+for i in range(row):
+    data=list(map(int, input("3개 입력해라").split())) 
+    result[i]=data # 입력한 데이터 값을 각 행에다 넣어줌
+    result[i].sort() # 오름차순 정렬 
+    
+    if(max<result[i][0]):  
+        max=result[i][0] #가장 작은 값 중에 큰 값 선택
+
+print(max)
