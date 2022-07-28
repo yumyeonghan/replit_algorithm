6bc368ffd7e33bec4b3512cb18ea9e3882b93a98
mgtmh991013 <>
2022-07-28 13:08:19 +0000

    문제풀이

--- 2_weak_implementation/ex06.py
+++ 2_weak_implementation/ex06.py
@@ -3,7 +3,7 @@
 list=list(input())
 index=0
 sum=0
-list.sort() # 정렬하면 숫자 영어 순으로 정렬됨
+list.sort() # 정렬하면 숫자 영어 순으로 정렬됨.
 
 for i in range(len(list)):
       if list[i]=='1'or list[i]=='2' or list[i]=='3' or list[i]=='4' or list[i]=='5' or list[i]=='6' or list[i]=='7' or list[i]=='8' or               list[i]=='9':
